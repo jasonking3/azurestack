ad5e07670c1fa0e5bc814e4117fc45fa16e7ed3b
Jason King <jasonkin@cisco.com>
2018-10-18 15:36:05 -0700

    Added ad_ds_install.ps1.  Removed ad_install.zip

---
+++ ad_ds_install.ps1
@@ -0,0 +1,64 @@
+configuration ADInstall
+{
+    param 
+    ( 
+        [Parameter(Mandatory)] 
+        [string] $safemodePassword, 
+        [Parameter(Mandatory)] 
+        [string] $domainPassword
+        # [PSCredential] $domainCred,
+        # [PSCredential] $safemodeAdministratorCred
+        # [Parameter(Mandatory)] 
+        # [pscredential]$DNSDelegationCred, 
+        # [Parameter(Mandatory)] 
+        # [pscredential]$NewADUserCred 
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, xActiveDirectory
+    
+    $secureSafemodePassword = ConvertTo-SecureString $safemodePassword -AsPlainText -Force
+    [PSCredential]$safemodeAdministratorCred = New-Object System.Management.Automation.PSCredential ("Administrator", $secureSafemodePassword)
+
+    $secureDomainPassword = ConvertTo-SecureString $domainPassword -AsPlainText -Force
+    [PSCredential]$domainCred = New-Object System.Management.Automation.PSCredential ("Administrator", $secureDomainPassword)
+
+    node "localhost"
+    {
+        LocalConfigurationManager
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+
+        # Install Domain Services role
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+        }
+
+        # Optional GUI tools            
+        WindowsFeature ADDSTools            
+        {             
+            Ensure = "Present"             
+            Name = "RSAT-ADDS"             
+        }            
+       
+        xADDomain FirstDS
+        {
+            DomainName = "ad.ciscops.net" 
+            DomainAdministratorCredential = $domainCred 
+            SafemodeAdministratorPassword = $safemodeAdministratorCred 
+            # DnsDelegationCredential = $DNSDelegationCred 
+            DependsOn = "[WindowsFeature]ADDSInstall" 
+        }
+    }
+}
+
+$ConfigData = @{
+    AllNodes = @(
+        @{ NodeName="localhost"; PSDscAllowPlainTextPassword = $true }
+)}
+
+ADInstall -ConfigurationData $ConfigData
