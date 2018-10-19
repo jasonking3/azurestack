4ee247ab696c6981a9d6315fddbe93498f95428b
Jason King <jasonkin@cisco.com>
2018-10-19 15:06:17 -0700

    Update ad_ds_install.ps1

--- ad_ds_install.ps1
+++ ad_ds_install.ps1
@@ -24,13 +24,6 @@ configuration ADInstall
 
     node "localhost"
     {
-        LocalConfigurationManager
-        {            
-            ActionAfterReboot = 'ContinueConfiguration'
-            ConfigurationMode = 'ApplyOnly'
-            RebootNodeIfNeeded = $true
-        }
-
         # Install Domain Services role
         WindowsFeature ADDSInstall
         {
